3d1eb04314125a0c7f94bb5371b4ccc02de6634a
Doroteo Vidal <doroteo17vidal@hotmail.com>
2023-10-21 10:52:33 -0300

    se agrega clase mantenimiento en microservicio-mantenimiento

--- microservicio-mantenimiento/src/main/java/com/tudai/aw/ms_mantenimiento/model/Mantenimiento.java
+++ microservicio-mantenimiento/src/main/java/com/tudai/aw/ms_mantenimiento/model/Mantenimiento.java
@@ -1,5 +1,28 @@
 package com.tudai.aw.ms_mantenimiento.model;
 
-public class Mantenimiento {
+import java.sql.Date;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+public class Mantenimiento {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(nullable = false)
+	private Date fechaHoraInicio;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(nullable = false)
+	private Date fechaHoraFin;
+	
+	@Column(name = "id_monopatin")
+	private Long idMonopatin;
 }
