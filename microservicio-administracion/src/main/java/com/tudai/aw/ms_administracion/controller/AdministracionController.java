b6537f4c6dc3ebeb107d4709ad75f99908e02f62
Doroteo Vidal <doroteo17vidal@hotmail.com>
2023-10-24 23:28:16 -0300

    se agrega servicio para administrador

---
+++ microservicio-administracion/src/main/java/com/tudai/aw/ms_administracion/controller/AdministracionController.java
@@ -0,0 +1,52 @@
+package com.tudai.aw.ms_administracion.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tudai.aw.ms_administracion.model.clases.Gps;
+import com.tudai.aw.ms_administracion.model.clases.Monopatin;
+import com.tudai.aw.ms_administracion.model.clases.Parada;
+import com.tudai.aw.ms_administracion.service.AdministracionService;
+
+@RestController
+@RequestMapping("administradores")
+public class AdministracionController {
+	
+	@Autowired
+	private AdministracionService administracionService;
+	
+	@PostMapping("/monopatines")
+    public ResponseEntity<?> agregarMonopatin(@RequestBody Monopatin monopatin) {
+		return ResponseEntity.status(HttpStatus.OK).body(administracionService.agregarMonopatin(monopatin));       
+    }
+	
+	@PutMapping("/monopatines/{id}")
+    public ResponseEntity<?> agregarMonopatinAMantenimiento(@PathVariable Long id) {
+		return ResponseEntity.status(HttpStatus.OK).body(administracionService.agregarMonopatinAMantenimiento(id));       
+    }
+	
+	@DeleteMapping("/monopatines/{id}")
+	public ResponseEntity<?> eliminarMonopatin(@PathVariable Long id) {
+		return administracionService.eliminarMonopatin(id);
+	}
+	
+	@PostMapping("/gps")
+    public ResponseEntity<?> agregarGps(@RequestBody Gps gps) {
+        return ResponseEntity.status(HttpStatus.OK).body(administracionService.agregarGps(gps));      
+    }
+	
+	@PostMapping("/paradas")
+    public ResponseEntity<?> agregarParada(@RequestBody Parada parada) {
+        return ResponseEntity.status(HttpStatus.OK).body(administracionService.agregarParada(parada));       
+    }
+	
+	//Agregar metodos propios de administrador get, post, put, delete
+}
---
+++ microservicio-administracion/src/main/java/com/tudai/aw/ms_administracion/service/AdministracionService.java
@@ -0,0 +1,121 @@
+package com.tudai.aw.ms_administracion.service;
+import java.time.Instant;
+import java.sql.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.tudai.aw.ms_administracion.model.clases.Gps;
+import com.tudai.aw.ms_administracion.model.clases.Mantenimiento;
+import com.tudai.aw.ms_administracion.model.clases.Monopatin;
+import com.tudai.aw.ms_administracion.model.clases.Parada;
+
+@Service("AdministracionService")
+public class AdministracionService {
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+    public ResponseEntity<?> agregarMonopatin(Monopatin monopatin) {
+    	HttpHeaders headers = new HttpHeaders();
+    	Monopatin monopatinNuevo = new Monopatin(monopatin);
+		HttpEntity<Monopatin> requestEntity = new HttpEntity<>(monopatinNuevo, headers);
+		
+		ResponseEntity<Monopatin> response = restTemplate.exchange(
+				"http://localhost:8011/monopatines/",
+				HttpMethod.POST,
+				requestEntity,
+				new ParameterizedTypeReference<Monopatin>() {}
+		);
+		
+		return response;
+    }
+	
+    public ResponseEntity<?> agregarGps(Gps gps) {
+    	// TODO Auto-generated method stub
+    	return null;
+    }
+
+	public ResponseEntity<?> agregarMonopatinAMantenimiento(Long id) {
+		HttpHeaders headers = new HttpHeaders();
+		HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+		
+		ResponseEntity<Monopatin> response = restTemplate.exchange(
+				"http://localhost:8011/monopatines/" + id, 
+				HttpMethod.GET, 
+				requestEntity, 
+				new ParameterizedTypeReference<Monopatin>() {} //convierte en Json
+		);
+		
+		if(response.getStatusCode().is2xxSuccessful()) {
+			Monopatin monopatin = response.getBody();
+			
+			if(monopatin.getEstado().equals("disponible")) {
+				this.agregarMantenimiento(id);
+				monopatin.setEstado("en mantenimiento");
+				HttpEntity<Monopatin> requestEntity2 = new HttpEntity<>(monopatin, headers);
+				ResponseEntity<Monopatin> response2 = restTemplate.exchange(
+						"http://localhost:8011/monopatines/" + id,
+						HttpMethod.PUT,
+						requestEntity2,
+						new ParameterizedTypeReference<Monopatin>() {}
+				);
+				
+				return response2;
+			}
+		}
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se encontro el monopatin que quiere agregar a mantenimiento");
+	}
+
+	private ResponseEntity<?> agregarMantenimiento(Long id) {
+		HttpHeaders headers = new HttpHeaders();
+		
+		Mantenimiento mantenimiento = new Mantenimiento();
+		mantenimiento.setIdMonopatin(id);
+		mantenimiento.setFechaHoraInicio(Date.from(Instant.now()));
+		mantenimiento.setFechaHoraFin(null);
+		mantenimiento.setReparado(false);
+		
+		HttpEntity<Mantenimiento> requestEntity = new HttpEntity<>(mantenimiento, headers);
+		
+		ResponseEntity<Mantenimiento> response = restTemplate.exchange(
+				"http://localhost:8041/mantenimientos/",
+				HttpMethod.POST,
+				requestEntity,
+				new ParameterizedTypeReference<Mantenimiento>() {}
+		);
+		
+		return response;
+	}
+
+	public ResponseEntity<?> eliminarMonopatin(Long id) {
+		HttpHeaders headers = new HttpHeaders();
+		HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+		
+		ResponseEntity<String> response = restTemplate.exchange(
+				"http://localhost:8011/monopatines/" + id,
+				HttpMethod.DELETE,
+				requestEntity,
+				String.class
+		);
+		
+		if(response != null) {
+			return ResponseEntity.ok("Se elimino el monopatin con exito");
+		}
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se encontro el monopatin que quiere eliminar");
+	}
+
+	public ResponseEntity<?> agregarParada(Parada parada) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
