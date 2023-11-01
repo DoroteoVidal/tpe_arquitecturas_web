347b94217d3beef1a15e56c1f320879a54751292
Doroteo Vidal <doroteo17vidal@hotmail.com>
2023-11-01 15:04:16 -0300

    modificacion fecha vigencia de tarifas en carga de datos

--- microservicio-administracion/src/main/java/com/tudai/aw/ms_administracion/utils/CargaDatos.java
+++ microservicio-administracion/src/main/java/com/tudai/aw/ms_administracion/utils/CargaDatos.java
@@ -15,9 +15,9 @@ public class CargaDatos {
 	private TarifaRepository tarifaRepository;
 	
 	public void cargar() {
-		Tarifa t1 = new Tarifa("tarf02-21", 6.3, 2.8, LocalDate.of(2023, 10, 31));
-		Tarifa t2 = new Tarifa("tarf02-22", 6.9, 3.1, LocalDate.of(2023, 11, 2));
-		Tarifa t3 = new Tarifa("tarf02-23", 7.5, 3.9, LocalDate.of(2023, 11, 14));
+		Tarifa t1 = new Tarifa("tarf02-21", 6.3, 2.8, LocalDate.of(2023, 8, 2));
+		Tarifa t2 = new Tarifa("tarf02-22", 6.9, 3.1, LocalDate.of(2023, 9, 4));
+		Tarifa t3 = new Tarifa("tarf02-23", 7.5, 3.9, LocalDate.of(2023, 10, 4));
 		
 		tarifaRepository.save(t1);
 		tarifaRepository.save(t2);
